b704a7b03eb6bfbbb214cd2a4472a78db05f03c8
Alejandro Fabra <alfaseart@gmail.com>
2024-01-03 16:42:00 +0100

    choose cups and simulate generation

--- frontend/src/app/features/communities/communities-form/communities-form.component.ts
+++ frontend/src/app/features/communities/communities-form/communities-form.component.ts
@@ -1,9 +1,9 @@
-import {Component, OnInit, ViewChild} from '@angular/core';
-import { FormBuilder, FormControl, Validators } from "@angular/forms";
-import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import {ChangeDetectorRef, Component, OnInit, ViewChild} from '@angular/core';
+import {FormBuilder, FormControl, Validators} from "@angular/forms";
+import {NgbActiveModal} from '@ng-bootstrap/ng-bootstrap';
 import Swal from 'sweetalert2';
-import { Observable } from 'rxjs';
-import { CommunitiesApiService } from '../communities.service';
+import {Observable} from 'rxjs';
+import {CommunitiesApiService} from '../communities.service';
 import moment from 'moment';
 import {CustomersService} from "../../../core/core-services/customers/customers.service";
 import {EnergyService} from "../../../core/core-services/energy/energy.service";
@@ -14,7 +14,12 @@ import Chart from "chart.js/auto";
   templateUrl: './communities-form.component.html',
   styleUrls: ['./communities-form.component.scss'],
 })
-export class CommunitiesFormComponent implements OnInit{
+export class CommunitiesFormComponent implements OnInit {
+
+  //todo: si seleccionamos un cups para simular generacion y luego borramos community cups no se actualiza el chart
+  //todo: no se tiene en cuenta en la actualizacion la simulación.
+  //todo: si se ha seleccionado para la simulacion un cups y luego se quita de la comunidad, qué pasa?
+  //todo: a veces no cargan los cups de la comunidad aunque si cargan los cups.
 
   @ViewChild('yearChart') yearChart: any;
 
@@ -36,20 +41,23 @@ export class CommunitiesFormComponent implements OnInit{
     promotion: false,
   }
 
-  customers:any[]=[];
-  test: number=1;
+  customers: any;
+  test: number = 1;
   id: number = 0;
-  communityCups: any[]=[];
+  communityCups: any[] = [];
 
-  selectedCups:number=0;
-  sumImport:number=0;
-  sumGeneration:number=0;
-  sumConsumption:number=0;
-  sumExport:number=0;
+  selectedCups: any;
+  sumImport: number = 0;
+  sumGeneration: number = 0;
+  sumConsumption: number = 0;
+  sumExport: number = 0;
+  multiplyGenerationResult:number=0;
 
   yearlyChartCanvas: any;
   yearlyChartCanvasContent: any;
   yearlyChart: any;
+
+  communityId:number|any;
 
   form = this.formBuilder.group({
     id: new FormControl<number | null>(null),
@@ -58,22 +66,21 @@ export class CommunitiesFormComponent implements OnInit{
     createdAt: new FormControl<string | null>(null),
     updatedAt: new FormControl<string | null>(null),
   });
+
   constructor(
     private formBuilder: FormBuilder,
     private apiService: CommunitiesApiService,
     private activeModal: NgbActiveModal,
-    private customersService:CustomersService,
-    private energyService:EnergyService
-  ) {}
+    private customersService: CustomersService,
+    private energyService: EnergyService,
+    private cdr: ChangeDetectorRef
+  ) {
+  }
 
   ngOnInit() {
     this.yearlyChartCanvas = document.getElementById('yearly-chart');
     this.yearlyChartCanvasContent = this.yearlyChartCanvas.getContext('2d');
     this.getInfo()
-  }
-
-  changeTest(event:any){
-    console.log(event)
   }
 
   setEditingId(id: number) {
@@ -84,6 +91,7 @@ export class CommunitiesFormComponent implements OnInit{
     this.apiService.getById(id).subscribe((data) => {
       console.log(data)
       this.form.controls.id.setValue(data.id);
+      this.communityId = data.id;
       this.form.controls.name.setValue(data.name);
       this.form.controls.test.setValue(data.test);
       this.test = data.test;
@@ -92,23 +100,75 @@ export class CommunitiesFormComponent implements OnInit{
     });
   }
 
-  getInfo(){
+  getInfo() {
     this.customersService.getCustomersCups().subscribe(async (res: any) => {
+      let communityId = this.form.controls.id.getRawValue()
       this.customers = res.data[0];
-      console.log("cups: ",this.customers)
-      this.communityCups =  this.customers.filter((cups:any) =>
-        cups.community_id==this.form.controls.id.getRawValue()
+      console.log("cups: ", this.customers)
+      this.communityCups = this.customers.filter((cups: any) =>
+        cups.community_id == communityId
       )
-
-      this.communityCups.forEach(async (cups:any) =>{
-        let yearEnergy:any = await this.getYearEnergyByCups(cups.id,2023);
-        cups.yearEnergy = yearEnergy;
-        console.log("year energy",yearEnergy)
-        this.sumImport+=yearEnergy.sumImport | 0,this.sumGeneration+=yearEnergy.sumGeneration | 0,this.sumConsumption+=yearEnergy.sumConsumption| 0,this.sumExport+=yearEnergy.sumExport| 0;
-        this.updateYearChart()
-      })
-      console.log("community cups: ",this.communityCups)
+      console.log("communityCups", this.communityCups)
+      this.getCommunityEnergy()
+      // Notificar a ng-select que ha habido cambios
+      this.cdr.detectChanges();
     })
+  }
+
+  async multiplyGeneration(event:any){
+
+    this.multiplyGenerationResult = this.selectedCups.yearEnergy.sumGeneration*event.target.value;
+
+    this.sumImport = 0;
+    this.sumConsumption = 0;
+    this.sumGeneration = 0;
+    this.sumExport = 0;
+
+    const getAllEnergy = this.communityCups.map(async (cups: any) => {
+      this.sumImport += cups.yearEnergy.sumImport | 0;
+      if(cups.id == this.selectedCups.id){
+        this.sumGeneration += this.multiplyGenerationResult;
+      }else{
+        this.sumGeneration += cups.yearEnergy.sumGeneration | 0;
+      }
+
+      this.sumConsumption += cups.yearEnergy.sumConsumption | 0;
+      this.sumExport += cups.yearEnergy.sumExport | 0;
+    })
+    await Promise.all(getAllEnergy)
+    console.log(this.sumImport, this.sumConsumption, this.sumGeneration, this.sumExport)
+    this.updateYearChart()
+  }
+
+  changeSelectedCups(selectedCups: any) {
+    console.log("ep: ", selectedCups)
+    console.log("change cups to: ", this.selectedCups)
+  }
+
+  changeCommunityCups(communityCups: any) {
+    console.log("change community cups : ", communityCups, this.communityCups)
+    this.getCommunityEnergy();
+  }
+
+  async getCommunityEnergy() {
+    this.sumImport = 0;
+    this.sumConsumption = 0;
+    this.sumGeneration = 0;
+    this.sumExport = 0;
+    const getAllEnergy = this.communityCups.map(async (cups: any) => {
+      //todo harcoded year
+      let yearEnergy: any = await this.getYearEnergyByCups(cups.id, 2023);
+      cups.yearEnergy = yearEnergy;
+      //console.log("year energy",yearEnergy)
+      this.sumImport += yearEnergy.sumImport | 0;
+      this.sumGeneration += yearEnergy.sumGeneration | 0;
+      this.sumConsumption += yearEnergy.sumConsumption | 0;
+      this.sumExport += yearEnergy.sumExport | 0;
+    })
+    await Promise.all(getAllEnergy)
+    console.log(this.sumImport, this.sumConsumption, this.sumGeneration, this.sumExport)
+    this.updateYearChart()
+    console.log("community cups: ", this.communityCups)
   }
 
   save() {
@@ -141,20 +201,17 @@ export class CommunitiesFormComponent implements OnInit{
 
   getValues(): any {
     const values: any = {};
-
     values.id = this.form.value.id;
     values.name = this.form.value.name;
     values.test = this.form.value.test;
     values.createdAt = this.form.value.createdAt;
     values.updatedAt = this.form.value.updatedAt;
-
     return values;
   }
 
-  getYearEnergyByCups(cups: number,year: number): Promise<any> {
+  getYearEnergyByCups(cups: number, year: number): Promise<any> {
     return new Promise((resolve, reject) => {
       this.energyService.getYearByCups(year, cups!).subscribe((res: any) => {
-        console.log(res.data)
         let monthlyCupsData = res.data;
         let months: string[] = monthlyCupsData.map((entry: any) => entry.month);
         let kwhImport: number[] = monthlyCupsData.map((entry: any) => entry.import);
@@ -165,22 +222,31 @@ export class CommunitiesFormComponent implements OnInit{
         const sumGeneration = kwhGeneration.reduce((partialSum: number, a: number) => partialSum + (a | 0), 0);
         const sumConsumption = kwhConsumption.reduce((partialSum: number, a: number) => partialSum + (a | 0), 0);
         const sumExport = kwhExport.reduce((partialSum: number, a: number) => partialSum + (a | 0), 0);
-        let yearEnergy = {months, kwhImport, kwhGeneration,kwhConsumption,kwhExport,sumImport,sumGeneration,sumConsumption,sumExport}
+        console.log("cups: ", cups, "year energy ", sumImport, sumExport, sumConsumption, sumGeneration)
+        let yearEnergy = {
+          months,
+          kwhImport,
+          kwhGeneration,
+          kwhConsumption,
+          kwhExport,
+          sumImport,
+          sumGeneration,
+          sumConsumption,
+          sumExport
+        }
         resolve(yearEnergy)
       })
     })
   }
 
   updateYearChart() {
-
     if (!this.yearlyChart) {
       this.yearlyChart = new Chart(this.yearlyChartCanvasContent, {type: 'pie', data: {labels: [], datasets: []}})
     }
-
     this.yearlyChart.data = {
-      labels: [`Import: ${this.sumImport} Kwh`, `Generation: ${this.sumGeneration} Kwh`,`Consumption: ${this.sumConsumption} Kwh`,`Surplus: ${this.sumExport} Kwh`],
+      labels: [`Import: ${this.sumImport} Kwh`, `Generation: ${this.sumGeneration} Kwh`, `Consumption: ${this.sumConsumption} Kwh`, `Surplus: ${this.sumExport} Kwh`],
       datasets: [{
-        data: [this.sumImport,this.sumGeneration,this.sumConsumption,this.sumExport],
+        data: [this.sumImport, this.sumGeneration, this.sumConsumption, this.sumExport],
         backgroundColor: [
           'rgb(255, 99, 132)',
           'rgb(54, 162, 235)',
@@ -189,9 +255,7 @@ export class CommunitiesFormComponent implements OnInit{
         ]
       }]
     }
-
     this.yearlyChart.update();
-
   }
 
 }

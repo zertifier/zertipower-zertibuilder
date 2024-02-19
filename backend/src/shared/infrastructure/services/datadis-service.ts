a24c6a1f326df04f073590ef8de3c9d1ff4effb1
Alejandro Fabra <alfaseart@gmail.com>
2024-02-19 16:33:56 +0100

    update datadis service

--- backend/src/shared/infrastructure/services/datadis-service.ts
+++ backend/src/shared/infrastructure/services/datadis-service.ts
@@ -2,6 +2,7 @@ import { Injectable } from "@nestjs/common";
 import axios from 'axios';
 import { MysqlService } from "src/shared/infrastructure/services/mysql-service/mysql.service";
 import mysql from "mysql2/promise";
+import moment from 'moment';
 
 
 interface supply {
@@ -37,8 +38,11 @@ export class DatadisService {
     loginData = {username:'g67684878',password:'acEM2020!'}
     token:any=undefined;
     supplies:supply[];
-    startDate='2023/11' //todo now
-    endDate='2024/02' //todo 5 days before
+
+    //see data from a week ago
+    startDate= moment().subtract(1, 'weeks').format('YYYY/MM'); 
+    endDate= moment().format('YYYY/MM');
+
     energyHourData:energyHourData[]=[];
     private conn: mysql.Pool;
 
@@ -46,23 +50,27 @@ export class DatadisService {
 
         this.conn = this.mysql.pool;
 
-        // this.login().then(async()=>{
-        //     await this.getSupplies();
-        //     for (const supply of this.supplies){
-        //         console.log(supply)
-        //         let consumptionCupsData:any = await this.getConsumptionData(supply.cups,supply.distributorCode,this.startDate,this.endDate,0,supply.pointType).catch((e)=>{
-        //             return e;
-        //         })
-        //         this.energyHourData = this.energyHourData.concat(consumptionCupsData) 
-        //     }
-        //     for (const energy of this.energyHourData){
-        //         //todo get cups id from cups
-        //         //todo check if hour is registered
-        //         //todo post if it isn't registered
-        //     }
-        //     this.energyHourData=[];
-        // })
+        /*
+         this.login().then(async()=>{
+             await this.getSupplies();
+             await this.checkCups();
+             for (const supply of this.supplies){
+                 console.log(supply)
+                 let consumptionCupsData:any = await this.getConsumptionData(supply.cups,supply.distributorCode,this.startDate,this.endDate,0,supply.pointType).catch((e)=>{
+                     return e;
+                 })
+                 this.energyHourData = this.energyHourData.concat(consumptionCupsData) 
+                 await getCupsEnergyData(cupsId,startDate,endDate) //todo check!
 
+             }
+             for (const energy of this.energyHourData){
+                 //todo get cups id from cups
+                 //todo check if hour is registered
+                 //todo post if it isn't registered
+             }
+             this.energyHourData=[];
+         })
+*/
     }
 
     async login(){
@@ -79,7 +87,6 @@ export class DatadisService {
 
     async getSupplies(){
         let config = {method:'get', maxBodyLength: Infinity, url: 'https://datadis.es/api-private/api/get-supplies', headers: { 'Authorization': `Bearer ${this.token}`}}
-
         return new Promise(async (resolve,reject)=>{
             let response:any = await axios.request(config).catch((e:any)=>{
                 console.log('error logging in', e)
@@ -87,7 +94,6 @@ export class DatadisService {
             });
             this.supplies=response.data;
             resolve(this.supplies);
-            
         })
     }
 
@@ -100,7 +106,7 @@ export class DatadisService {
      * @param meeasurementType 
      * @param pointType 
      */
-    async getConsumptionData(cups:string,distributorCode:number,startDate:string,endDate:string,meeasurementType:number,pointType:number){
+    async getConsumptionData(cups:string,distributorCode:number,startDate:string,endDate:string,measurementType:number,pointType:number){
 
         return new Promise(async (resolve,reject)=>{
 
@@ -110,7 +116,7 @@ export class DatadisService {
                 let config = {
                     method: 'get',
                     maxBodyLength: Infinity,
-                    url: `${baseUrl}cups=${cups}&distributorCode=${distributorCode}&startDate=${startDate}&endDate=${endDate}&measurementType=${meeasurementType}&pointType=${pointType}`,
+                    url: `${baseUrl}cups=${cups}&distributorCode=${distributorCode}&startDate=${startDate}&endDate=${endDate}&measurementType=${measurementType}&pointType=${pointType}`,
                     headers: { 
                         'Authorization': `Bearer ${this.token}`
                     }
@@ -133,7 +139,50 @@ export class DatadisService {
         })
     }
 
-        
+    async checkCups(){
+        return new Promise(async (resolve,reject)=>{
+        try{
+            const getCupsQuery = `SELECT * FROM cups`;
+            const insertCupsQuery = `INSERT INTO cups (cups,ubication) VALUES (?,?)`
+
+            let [ROWS] = await this.conn.query(getCupsQuery);
+            let dbCups = ROWS;
+
+            //insert cups if it isn't already registered
+            this.supplies.map(async cupsData=>{
+                let dbFound = dbCups.find((registeredCups:any)=>registeredCups.cups===cupsData.cups)
+                if(!dbFound){
+                    //todo: add address, province, municipality at db cups table
+                    await this.conn.query(insertCupsQuery,[cupsData.cups,cupsData.municipality]);
+                }
+            })
+
+            
+            let [allCups] = await this.conn.query(getCupsQuery);
+            dbCups = allCups;
+            resolve(dbCups);
+            
+          } catch (e:any) {
+            console.log("error getting energy areas", e);
+            reject(e)
+           
+          }
+        })
+    }
+
+    getCupsEnergyData(cupsId:number,startDate:string,endDate:string){
+        return new Promise(async (resolve,reject)=>{
+            try{
+                //todo: table is energy_registers_original_hourly (change database)
+                const getCupsEnergyDataQuery = `SELECT * FROM energy_registers_hourly where cups_id = ? and info_datetime IS BEETWEEN ? AND ?`;
+                let [ROWS] = await this.conn.query(getCupsEnergyDataQuery, [cupsId, startDate, endDate]);
+                resolve(ROWS);
+                } catch (e:any) {
+                console.log("error getting cups energy data", e);
+                reject(e)
+                }
+            })
+    }
 
         
 

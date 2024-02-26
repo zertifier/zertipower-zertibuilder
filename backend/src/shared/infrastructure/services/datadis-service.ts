6da29bffeac8e40b9b942e6a44a7c3b0a56e7704
Alejandro Fabra <alfaseart@gmail.com>
2024-02-26 16:35:48 +0100

    update datadis fluxe

--- backend/src/shared/infrastructure/services/datadis-service.ts
+++ backend/src/shared/infrastructure/services/datadis-service.ts
@@ -3,6 +3,7 @@ import axios from 'axios';
 import { MysqlService } from "src/shared/infrastructure/services/mysql-service/mysql.service";
 import mysql from "mysql2/promise";
 import moment from 'moment';
+import { throwError } from "rxjs";
 
 
 interface supply {
@@ -40,8 +41,7 @@ export class DatadisService {
     supplies:supply[];
 
     //see data from a week ago
-    startDate= ''//moment().subtract(1, 'weeks').format('YYYY/MM'); 
-    endDate= ''//moment().format('YYYY/MM');
+    
 
     energyHourData:energyHourData[]=[];
     private conn: mysql.Pool;
@@ -49,28 +49,46 @@ export class DatadisService {
     constructor(private mysql: MysqlService){
 
         this.conn = this.mysql.pool; 
+        
+        let startDate= moment().subtract(1, 'weeks').format('YYYY/MM'); 
+        let endDate= moment().format('YYYY/MM');
+        this.run
 
-        /*
-         this.login().then(async()=>{
-             await this.getSupplies();
-             await this.checkCups();
-             for (const supply of this.supplies){
-                 console.log(supply)
-                 let consumptionCupsData:any = await this.getConsumptionData(supply.cups,supply.distributorCode,this.startDate,this.endDate,0,supply.pointType).catch((e)=>{
-                     return e;
-                 })
-                 this.energyHourData = this.energyHourData.concat(consumptionCupsData) 
-                 await getCupsEnergyData(cupsId,startDate,endDate) //todo check!
+        setInterval(()=>{
+            startDate= moment().subtract(1, 'weeks').format('YYYY/MM'); 
+            endDate= moment().format('YYYY/MM');
+            this.run
+        },86400000) //24 h => ms
 
-             }
-             for (const energy of this.energyHourData){
-                 //todo get cups id from cups
-                 //todo check if hour is registered
-                 //todo post if it isn't registered
-             }
-             this.energyHourData=[];
-         })
-*/
+    }
+
+    async run(startDate:any,endDate:any){
+        //get auth token
+        this.login().then(async()=>{
+            //get datadis cups
+            await this.getSupplies();
+            //check if all cups are in database already
+            await this.checkCups();
+            //go across cups:
+            for (const supply of this.supplies){
+                
+                console.log(supply)
+
+                //obtain datadis hour energy:
+                let datadisCupsEnergyData:any = await this.getConsumptionData(supply.cups,supply.distributorCode,startDate,endDate,0,supply.pointType).catch((e)=>{
+                    return e;
+                })
+
+                console.log(JSON.stringify(datadisCupsEnergyData).substring(0,200))
+
+                //insert readed cups energy hours 
+                this.postLogs(supply.cups,datadisCupsEnergyData.length)
+             
+                //get cups energy hours db registers 
+                let databaseEnergyHourData = await this.postCupsEnergyData(supply.cups,datadisCupsEnergyData,startDate,endDate) //todo check!
+
+            }
+        })
     }
 
     async login(){
@@ -157,7 +175,6 @@ export class DatadisService {
                 }
             })
 
-            
             let [allCups] = await this.conn.query(getCupsQuery);
             dbCups = allCups;
             resolve(dbCups);
@@ -170,20 +187,106 @@ export class DatadisService {
         })
     }
 
-    getCupsEnergyData(cupsId:number,startDate:string,endDate:string){
+    async postCupsEnergyData(cups:string,datadisCupsEnergyData:any[],startDate:string,endDate:string){
+
+        //create get not inserted energy per cups query
+
+        let dataToSearchQueryPart = ''
+        let values:any = []
+        let firstEnergyDate = datadisCupsEnergyData[0]
+        datadisCupsEnergyData.slice(1)
+        
+        let day = moment(firstEnergyDate.date ).format('DD-MM-YYYY')
+        let hour = moment(firstEnergyDate.time).format('HH:mm:ss') 
+        let datetime = `${day} ${hour}`;
+        let consumption = firstEnergyDate.consumptionKWh;
+        let generation = firstEnergyDate.surplusEnergyKWh;
+
+        //get cups id
+        let cupsId:any;
+        try{
+            let getCupsIdQuery  =`SELECT id FROM cups WHERE cups = ?`;
+            let [ROWS]:any = await this.conn.query(getCupsIdQuery, [cups]);
+            cupsId = ROWS[0].id;
+        } catch (e:any) {
+            console.log("error getting cups energy data", e);
+            throwError(e)
+        }
+
+        dataToSearchQueryPart.concat(`SELECT ? as info_datetime, ? as consumption, ? as production, ? as cups_id`)
+
+        values.push(datetime); values.push(consumption); values.push(generation);values.push(cupsId)
+
+        datadisCupsEnergyData.forEach((energy,index) => {
+            
+            let day = moment(energy.date ).format('DD-MM-YYYY')
+            let hour = moment(energy.time).format('HH:mm:ss') 
+
+            let datetime = `${day} ${hour}`;
+            let consumption = energy.consumptionKWh;
+            let generation = energy.surplusEnergyKWh;
+
+            values.push(datetime);
+            values.push(consumption);
+            values.push(generation)
+            values.push(cupsId);
+            
+            dataToSearchQueryPart.concat(`UNION ALL SELECT ?,?,?`)
+
+        })
+
+        let query = ` 
+        INSERT INTO energy_registers (info_dt, generation, consumption,cups_id) 
+        SELECT info_datetime, generation, consumption, cups_id
+        FROM ( ${dataToSearchQueryPart} ) AS data_to_check 
+        WHERE data_to_check.info_datetime
+        NOT IN (
+        SELECT info_datetime
+        FROM energy_registers
+        WHERE info_datetime 
+        BETWEEN ? AND ?  
+        )
+        AND cups_id = ?
+        ` 
+
+        values.push(startDate); values.push(endDate);values.push(cupsId);
+
         return new Promise(async (resolve,reject)=>{
-            try{
+
+            /*try{
                 //todo: table is energy_registers_original_hourly (change database)
-                const getCupsEnergyDataQuery = `SELECT * FROM energy_registers_original_hourly where cups_id = ? and info_datetime IS BEETWEEN ? AND ?`;
-                let [ROWS] = await this.conn.query(getCupsEnergyDataQuery, [cupsId, startDate, endDate]);
+                const getCupsEnergyDataQuery = `SELECT energy_registers_original_hourly.*, cups FROM energy_registers_original_hourly LEFT join cups ON cups.id = cups_id where cups = ? and info_datetime IS BEETWEEN ? AND ?`;
+                let [ROWS] = await this.conn.query(getCupsEnergyDataQuery, [cups, startDate, endDate]);
                 resolve(ROWS);
-                } catch (e:any) {
+            } catch (e:any) {
                 console.log("error getting cups energy data", e);
                 reject(e)
-                }
-            })
+            }*/
+
+            try{
+                let [ROWS] = await this.conn.query(query, values);
+                resolve(ROWS);
+            }catch(e:any){
+                console.log("error putting cups energy data", e);
+                reject(e)
+            }
+
+        })
+
     }
 
+    postLogs(cups:string, n_registers:number){
+        const insertLogsQuery = `INSERT INTO logs (cups,n_registers) VALUES (?,?)`
+        return new Promise(async (resolve,reject)=>{
+            try{
+                let [ROWS] = await this.conn.query(insertLogsQuery, [cups, n_registers]);
+                resolve(ROWS);
+            } catch (e:any) {
+                console.log("error setting logs energy data", e);
+                reject(e)
+            }
+        })
+    }
         
 
 }

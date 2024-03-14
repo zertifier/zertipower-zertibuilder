4e226e7720ba3a70e8655f0f443452262da34ce7
Alejandro Fabra <alfaseart@gmail.com>
2024-03-14 17:28:03 +0100

    Update datadis-service.ts

--- backend/src/shared/infrastructure/services/datadis-service.ts
+++ backend/src/shared/infrastructure/services/datadis-service.ts
@@ -75,7 +75,7 @@ export class DatadisService {
         setInterval(()=>{
             startDate= moment().subtract(1, 'months').format('YYYY/MM');
             endDate= moment().format('YYYY/MM');
-            //this.run(startDate,endDate)
+            this.run(startDate,endDate)
         },86400000) //24 h => ms
 
     }

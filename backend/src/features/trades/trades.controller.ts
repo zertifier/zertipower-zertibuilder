e406220d5fe5393809e468bccad8c1a422f811e8
alex <alexlopezpal96@gmail.com>
2024-07-10 14:12:41 +0200

    updated: getByDateAndCustomer from trades now get only from_cups_id

--- backend/src/features/trades/trades.controller.ts
+++ backend/src/features/trades/trades.controller.ts
@@ -33,11 +33,19 @@ export class TradesController {
 
   @Get('/customer/:customerId/date/:from/:to')
   // @Auth(RESOURCE_NAME)
-  async getByDateAndCustomer(@Param('customerId') customerId: string, @Param('from') from: string, @Param('to') to: string, ) {
-    const query = `
+  async getByDateAndCustomer(@Param('customerId') customerId: string, @Param('from') from: string, @Param('to') to: string) {
+ /*   const query = `
       SELECT trades.id, action, traded_kwh, cost, previous_kwh, current_kwh, info_dt
       FROM trades
              LEFT JOIN cups ON trades.from_cups_id = cups.id OR trades.to_cups_id = cups.id
+      WHERE cups.customer_id = ${customerId}
+        AND DATE(trades.info_dt) BETWEEN '${from}' AND '${to}'
+      ORDER BY info_dt DESC
+    `*/
+    const query = `
+      SELECT trades.id, action, traded_kwh, cost, previous_kwh, current_kwh, info_dt
+      FROM trades
+             LEFT JOIN cups ON trades.from_cups_id = cups.id
       WHERE cups.customer_id = ${customerId}
         AND DATE(trades.info_dt) BETWEEN '${from}' AND '${to}'
       ORDER BY info_dt DESC

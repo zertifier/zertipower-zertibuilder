bf51ff4fb6ee057edc16b02798eb2abd6006b1cb
Alejandro Fabra <alfaseart@gmail.com>
2024-07-23 15:04:02 +0200

    feature refresh permanent token on realtime

--- real-time-energy-api/controllers/tokens.js
+++ real-time-energy-api/controllers/tokens.js
@@ -2,43 +2,63 @@ const { response } = require("express");
 const { dbConnection } = require('../database/config');
 const { generatePermaJWT } = require("../utils/middlewares/jwt");
 
-const generatePermaToken = async (req, res = response) => {
+const refreshPermaToken = async (req, res = response) => {
     try {
-        
         let userId = req.uid;
         let customerId;
-
         let [ROWS] = await dbConnection.execute
             (`SELECT customer_id FROM users WHERE id = ?`, [userId])
-        
-            if (!ROWS[0]) {
-            console.log("customer not found")
+        if (!ROWS[0]) {
             return res.status(404).json({
                 ok: false,
                 msg: 'customer not found'
             })
         }
-        
         customerId = ROWS[0].customer_id;
+        [ROWS] = await dbConnection.execute
+            (`DELETE FROM user_tokens WHERE user_id = ? and permanent = 1`, [userId])
+        token = await generatePermaJWT(customerId, userId);
+        return res.json({
+            ok: true,
+            token: `Bearer ${token}`
+        });
+    } catch (error) {
+        console.log("Error refreshing token", error)
+        res.status(500).json({
+            ok: false,
+            msg: error
+        })
+    }
+}
 
+const generatePermaToken = async (req, res = response) => {
+    try {
+        let userId = req.uid;
+        let customerId;
+        let [ROWS] = await dbConnection.execute
+            (`SELECT customer_id FROM users WHERE id = ?`, [userId])
+        if (!ROWS[0]) {
+            return res.status(404).json({
+                ok: false,
+                msg: 'customer not found'
+            })
+        }
+        customerId = ROWS[0].customer_id;
         [ROWS] = await dbConnection.execute
             (`SELECT token FROM user_tokens WHERE user_id = ? and permanent = 1`, [userId])
         if (!ROWS[0]) {
-            console.log("generate token")
             token = await generatePermaJWT(customerId, userId);
             return res.json({
                 ok: true,
                 token: `Bearer ${token}`
             });
         }
-
-        console.log("token obtained")
         return res.json({
             ok: true,
             token: `Bearer ${ROWS[0].token}`
         });
     } catch (error) {
-        console.log("Error generating tokens", error)
+        console.log("Error generating token", error)
         res.status(500).json({
             ok: false,
             msg: error
@@ -93,5 +113,6 @@ const getToken = async (req, res = response) => {
 module.exports = {
     getToken,
     getTokens,
-    generatePermaToken
+    generatePermaToken,
+    refreshPermaToken
 }
--- real-time-energy-api/routes/tokens.js
+++ real-time-energy-api/routes/tokens.js
@@ -1,9 +1,11 @@
 const {Router} = require('express');
-const {generatePermaToken} = require("../controllers/tokens");
+const {generatePermaToken,refreshPermaToken} = require("../controllers/tokens");
 const { validateTempJWT } = require("../utils/middlewares/jwt");
 
 const router = Router();
 
 router.post('/',validateTempJWT,generatePermaToken)
+
+router.post('/refresh',validateTempJWT,refreshPermaToken)
 
 module.exports = router;

fb9a649c3e8e6f5cdb08edf2d341542b2a3e70f0
Jose David <josedvd123@gmail.com>
2024-09-12 12:12:21 +0200

    Fix: stats request

--- backend/src/features/communities/communities.controller.ts
+++ backend/src/features/communities/communities.controller.ts
@@ -208,7 +208,8 @@ export class CommunitiesController {
              a.surplus_community
       FROM (SELECT SUM(kwh_in)                                       AS kwh_in,
                    SUM(eh.kwh_out)                                   AS kwh_out,
-                   SUM(kwh_out_virtual)                              AS kwh_out_virtual,
+                   SUM(IFNULL(kwh_in_virtual, kwh_in))                              AS kwh_in_virtual,
+                   SUM(IFNULL(kwh_out_virtual, kwh_out))                              AS kwh_out_virtual,
                    (SUM(COALESCE(kwh_in, 0)) + SUM(COALESCE(kwh_out, 0)))                 AS kwh_total,
                    (SUM(COALESCE(kwh_in_virtual, 0)) + SUM(COALESCE(kwh_out_virtual, 0))) AS kwh_virtual_total,
                    100 - (SUM(COALESCE(kwh_in_virtual, 0)) + SUM(COALESCE(kwh_out_virtual, 0))) * 100.0 /
@@ -336,7 +337,8 @@ export class CommunitiesController {
       FROM (SELECT SUM(kwh_in)                                       AS kwh_in,
                    SUM(kwh_in_virtual)                              AS kwh_in_virtual,
                    SUM(eh.kwh_out)                                   AS kwh_out,
-                   SUM(kwh_out_virtual)                              AS kwh_out_virtual,
+                   SUM(IFNULL(kwh_in_virtual, kwh_in))                              AS kwh_in_virtual,
+                   SUM(IFNULL(kwh_out_virtual, kwh_out))                              AS kwh_out_virtual,
                    (SUM(COALESCE(kwh_in, 0)) + SUM(COALESCE(kwh_out, 0)))                 AS kwh_total,
                    (SUM(COALESCE(kwh_in_virtual, 0)) + SUM(COALESCE(kwh_out_virtual, 0))) AS kwh_virtual_total,
                    100 - (SUM(COALESCE(kwh_in_virtual, 0)) + SUM(COALESCE(kwh_out_virtual, 0))) * 100.0 /

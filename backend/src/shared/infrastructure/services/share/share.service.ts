0457132cf7b8f5ad630dac1e94b4e3fd72a53eab
alex <alexlopezpal96@gmail.com>
2024-09-20 16:18:54 +0200

    fix: share service

--- backend/src/shared/infrastructure/services/share/share.service.ts
+++ backend/src/shared/infrastructure/services/share/share.service.ts
@@ -370,8 +370,7 @@ export class ShareService {
                  LEFT JOIN cups ON e.cups_id = cups.id
                  LEFT JOIN users ON cups.customer_id = users.customer_id
                  LEFT JOIN communities ON cups.community_id = communities.id
-          WHERE e.info_dt LIKE '2024-02-20%'
-            AND cups.type != 'community'
+          WHERE  cups.type != 'community'
             AND e.kwh_out > 0
           ORDER BY e.info_dt ASC, e.kwh_out DESC;
         `
@@ -423,8 +422,7 @@ export class ShareService {
                LEFT JOIN cups ON e.cups_id = cups.id
                LEFT JOIN users ON cups.customer_id = users.customer_id
                LEFT JOIN communities ON cups.community_id = communities.id
-        WHERE e.info_dt LIKE '2024-02-20%'
-          AND cups.type != 'community'
+        WHERE cups.type != 'community'
           AND e.kwh_in > 0
         ORDER BY e.info_dt ASC, e.kwh_in DESC;
       `)
